ec091b93d19bd181d42dd855bb8f053109b6b5ed
Aleksandr Bazhin <aleksandrbazhin@gmail.com>
2020-10-23 20:12:03 +1000

    cleanup junk

--- addons/PerfMonitor/plugin.gd
+++
@@ -1,8 +0,0 @@
-tool
-extends EditorPlugin
-
-func _enter_tree():
-	add_custom_type("Monitor", "CanvasLayer", preload("PerfMonitor.gd"), preload("icon.png"))
-
-func _exit_tree():
-	remove_custom_type("Monitor")
