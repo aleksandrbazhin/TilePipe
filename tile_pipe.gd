f8a0be1fc566e627ef860e7aba7bc857ff2292ab
aleksandrbazhin <aleksandrbazhin@gmail.com>
2021-05-01 23:58:17 +0300

    Fix: Mac default paths

--- tile_pipe.gd
+++ tile_pipe.gd
@@ -229,11 +229,18 @@ func load_template_texture(path: String) -> String:
 	template_texture_name.text =  template_name
 	return path
 
+func get_default_dir_path() -> String:
+#	report_error(OS.get_name()+"  "+OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS))
+	if OS.get_name() == "OSX":
+		return OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
+	else:
+		return OS.get_executable_path().get_base_dir()
+	
 func clear_path(path: String) -> String:
 	if path.begins_with("res://"):
-		return OS.get_executable_path().get_base_dir() + "/" + path.get_file()
+		return get_default_dir_path() + "/" + path.get_file()
 	else:
-		return path 
+		return path
 
 func apply_saved_settings(data: Dictionary):
 	generation_data = GenerationData.new(data["last_gen_preset_path"])
