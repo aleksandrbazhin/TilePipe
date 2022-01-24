12e5a83aba4e3d362ea5843baa1ec0842a9749e9
aleksandrbazhin <aleksandrbazhin@gmail.com>
2022-01-24 16:51:28 +0300

    New ruleset json format with JSON schema and validation

---
+++ rulesets/validate.py
@@ -0,0 +1,28 @@
+from xml.dom import ValidationErr
+from jsonschema import validate, ValidationError
+import json
+
+
+def validate_files():
+    schema_file = open('ruleset_schema.json')
+    schema = json.load(schema_file)
+    schema_file.close()
+
+    is_ok = True
+
+    for file_name in ['basic_4.json']:
+        json_file = open(file_name)
+        data = json.load(json_file)
+        json_file.close()
+
+        try:
+            validate(instance=data,  schema=schema)
+        except ValidationError as e:
+            is_ok = False
+            print( "Error validating \"" + file_name + "\":\n" + e.message + "\nPath in json:\n" + e.json_path)
+    if is_ok:
+        print("All rulesets are validated: OK")
+
+if __name__ == "__main__":
+    validate_files()
+
