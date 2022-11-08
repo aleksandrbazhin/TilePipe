8d1c8189d1254f3459c415012617cf6e35e1297a
aleksandrbazhin <aleksandrbazhin@gmail.com>
2022-11-09 00:06:45 +0700

    fix visual bug with ruleset preview pn main til view

--- src/nodes/TileMainView.gd
+++ src/nodes/TileMainView.gd
@@ -64,7 +64,7 @@ func add_ruleset_highlights(ruleset: Ruleset):
 		var highlight := preload("res://src/nodes/PartHighlight.tscn").instance()
 		ruleset_texture.add_child(highlight)
 		highlight.rect_position.x = i * (ruleset.PREVIEW_SIZE_PX + ruleset.PREVIEW_SPACE_PX)
-		highlight.set_id(i + 1)
+		highlight.set_id(i + 1, true)
 		highlight.connect("focused", self, "on_part_highlight_focused")
 		highlight.connect("unfocused", self, "on_part_highlight_unfocused")
 
@@ -168,7 +168,3 @@ func _on_TextureFileDialog_popup_hide():
 func _on_Godot3ExportDialog_popup_hide():
 	display_export_path(Const.EXPORT_TYPES.GODOT3)
 	State.popup_ended()
-
-#
-#func _on_Godot3ExportDialog_about_to_show():
-#	State.popup_started($Godot3ExportDialog)
